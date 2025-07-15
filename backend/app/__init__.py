cb08961240863c954d183de3e8e29c701ee075c3
NatalieGichuki <nataliegichuki@gmail.com>
2025-07-15 13:36:04 +0300

    modify __init__ file so as to sync with config and add comments for code understanding

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,26 +4,39 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 # Imports the config class dictionary (config_by_name) from config.py to load environment-specific configs.
-from app.config import Config
+from app.config import config_by_name
 
 # Initialize extensions
+# These create instances of Flask extensions without tying them to the app yet.
 db = SQLAlchemy()
 migrate = Migrate()
 jwt = JWTManager()
 cors = CORS()
 
-def create_app():
+# Defines a function that takes in a configuration name ("development", "testing", etc.).
+# This function creates and configures the Flask app based on the provided configuration.
+def create_app(config_name):
+    # Creates a new Flask application instance.
     app = Flask(__name__)
-    app.config.from_object(Config)
-
+    # Loads the configuration settings based on the config_name argument.
+    # This line makes .env variables like DATABASE_URL, JWT_SECRET_KEY, etc., available to the app.
+    app.config.from_object(config_by_name[config_name])
+    
+    # Initializes the Flask extensions with the app instance.
+    # Initializes the SQLAlchemy database connection with the Flask app.
     db.init_app(app)
+    # Initializes the Flask-Migrate extension for database migrations.
     migrate.init_app(app, db)
+    # Initializes the Flask-JWT-Extended extension for JWT authentication. Hence on can use JWT in their routes
     jwt.init_app(app)
+    # Initializes Flask-CORS to handle Cross-Origin Resource Sharing, allowing the app to accept requests from different origins.
     cors.init_app(app)
-
+    
+    # Create the database tables if they don't exist.
     with app.app_context():
+        # Import the routes and models to ensure they are registered with the app context.
         from app import routes, models
-        db.create_all()
+       
 
         # Register blueprints
 
