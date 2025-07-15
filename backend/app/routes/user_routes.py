6c52682d67f7307d4535676b87e744f2ab9c1847
NatalieGichuki <nataliegichuki@gmail.com>
2025-07-15 18:35:11 +0300

    Add user_routes file and define the admin route to view the customers

---
+++ backend/app/routes/user_routes.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.models.user import User
+from app.utils.auth_helpers import role_required
+
+user_bp = Blueprint('user', __name__)
+@user_bp.route('/users', methods=['GET'])
+@jwt_required()
+@role_required('admin')
+def get_users():
+    users = User.query.all()
+    return jsonify([{
+        "id": user.id,
+        "username": user.username,
+        "email": user.email,
+        "role": user.role
+    } for user in users]), 200
