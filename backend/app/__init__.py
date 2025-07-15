7cd15ef668e241a8894abe8752e0a8a52ffea540
NatalieGichuki <nataliegichuki@gmail.com>
2025-07-15 12:58:09 +0300

    Initialization in the init file on db migrations, cors and jwt

---
+++ backend/app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from app.config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager()
+cors = CORS()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+    cors.init_app(app)
+
+    with app.app_context():
+        from app import routes, models
+        db.create_all()
+
+        # Register blueprints
+
+    return app
