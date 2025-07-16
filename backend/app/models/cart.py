9d011f56a3dffbe6c5230db108022eb77153b8b4
Elviskiprono <biggeelvo831@gmail.com>
2025-07-16 14:23:13 +0300

    Add Cart model with user_id foreign key

---
+++ backend/app/models/cart.py
@@ -0,0 +1,10 @@
+from .. import db
+
+class Cart(db.Model):
+    __tablename__ = 'carts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Cart {self.id}>'
