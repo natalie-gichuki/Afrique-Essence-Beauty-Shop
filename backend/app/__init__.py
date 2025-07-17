f99e4168e5078b7cc71931ec7c59a33f9411f43c
NatalieGichuki <nataliegichuki@gmail.com>
2025-07-17 15:46:07 +0300

    Register category and product blueprints

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -33,7 +33,7 @@ def create_app(config_name = "development"):
     # Initializes Flask-CORS to handle Cross-Origin Resource Sharing, allowing the app to accept requests from different origins.
     cors.init_app(app)
     
-    from app.routes import auth_routes, user_routes, analytics_routes, export_routes, cart_routes, order_routes, invoice_routes
+    from app.routes import auth_routes, user_routes, analytics_routes, export_routes, cart_routes, order_routes, invoice_routes, category_routes, product_routes
     from app import models
        
 
@@ -45,6 +45,8 @@ def create_app(config_name = "development"):
     app.register_blueprint(cart_routes.cart_bp, url_prefix='/cart')
     app.register_blueprint(order_routes.order_item_bp, url_prefix='/orders')
     app.register_blueprint(invoice_routes.invoice_bp, url_prefix='/invoices')
+    app.register_blueprint(category_routes.category_bp, url_prefix='/categories')
+    app.register_blueprint(product_routes.product_bp, url_prefix='/products')
 
     return app
 
