5090575bf63110f36837ab65f7e4fc1e90b1a096
Elviskiprono <biggeelvo831@gmail.com>
2025-07-22 13:53:48 +0300

    add protected route for admin analytics page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,28 @@
-
+import AdminAnalytics from './pages/AdminAnalytics';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 import AdminDashboard from "./pages/AdminDashboard";
-
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Admin Dashboard Route */}
         <Route
           path="/admin"
           element={
             <ProtectedRoute adminOnly={true}>
               <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Admin Analytics Route */}
+        <Route
+          path="/admin/analytics"
+          element={
+            <ProtectedRoute adminOnly={true}>
+              <AdminAnalytics />
             </ProtectedRoute>
           }
         />
