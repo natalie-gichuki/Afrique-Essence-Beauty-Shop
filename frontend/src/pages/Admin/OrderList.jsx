fad1bb54d4c14786ce8fde2053a9a923d7b5632c
Elviskiprono <biggeelvo831@gmail.com>
2025-07-23 00:35:42 +0300

    Created OrderList admin page for viewing orders

---
+++ frontend/src/pages/Admin/OrderList.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+const OrderList = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-bold">Order Management</h2>
+      <p>Coming soon: list of all customer orders</p>
+    </div>
+  );
+};
+
+export default OrderList;
+// This page will eventually allow admins to view and manage customer orders in the system.
