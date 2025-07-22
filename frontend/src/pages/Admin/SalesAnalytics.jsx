0b95d9c8f82418400661ec0d17384ea0eb664a32
Elviskiprono <biggeelvo831@gmail.com>
2025-07-23 00:37:19 +0300

    Added SalesAnalytics page with embedded chart component

---
+++ frontend/src/pages/Admin/SalesAnalytics.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import Chart from "../../components/Chart";
+
+const SalesAnalytics = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-bold mb-4">Sales Analytics</h2>
+      <Chart />
+    </div>
+  );
+};
+
+export default SalesAnalytics;
+// This page will eventually display sales analytics data in a chart format.
